46e422d5f7243735f73b3c3c4723a5d360e39081
qudef <wkto981@gmail.com>
2025-12-15 09:30:12 +0300

    deleted cxx temp files

--- problem-solver/cxx/district-module/district_module.cpp
+++
@@ -1,9 +0,0 @@
-#include "district_module.hpp"
-#include "agents/bfs_agent.hpp"
-#include "agents/dfs_agent.hpp"
-#include "agents/create_graph_district_agent.hpp"
-#include "agents/bfs_agent.hpp"
-SC_MODULE_REGISTER(DistrictModule)
-    ->Agent<CreateGraphAgent>()
-    ->Agent<TransportNetDFSAgent>()
-    ->Agent<TransportNetBFSAgent>();
